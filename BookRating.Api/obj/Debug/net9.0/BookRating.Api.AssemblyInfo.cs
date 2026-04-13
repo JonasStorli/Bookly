cb3300dc743539fbbe4cda7d94d2b75c09941066
Jonas_Storli <sanoj.storli@gmail.com>
2026-04-13 12:44:03 +0200

    Update AssemblyInformationalVersion & build artifacts
    Regenerate build outputs: update AssemblyInformationalVersion to include new commit hash, add BookRating.Api.sourcelink.json, and refresh bin/obj artifacts (DLLs, PDBs and various cache/DSWA files). These are build-generated changes reflecting a new build at commit 266046127dad9499a14e3fd0b88a0e6af4eedbe2.

--- BookRating.Api/obj/Debug/net9.0/BookRating.Api.AssemblyInfo.cs
+++ BookRating.Api/obj/Debug/net9.0/BookRating.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookRating.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2baa33fb536f9ce166e788e0865271f150c8e15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266046127dad9499a14e3fd0b88a0e6af4eedbe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookRating.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookRating.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
